cbb80e65e82a4b8b438749026c70ceda6e4ecbae
Mehdi Maachou <mehdi.maachou@gmail.com>
2022-03-13 03:12:46 +0100

    adding graphql api + auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "todo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "todo680a7d95": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <authenticator>
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+        <h1>Todo App</h1>
+        <input type="text" v-model="name" placeholder="Todo name" />
+        <input type="text" v-model="description" placeholder="Todo description" />
+        <button v-on:click="createTodo">Create Todo</button>
+        <div v-for="item in todos" :key="item.id">
+          <h3>{{ item.name }}</h3>
+          <p>{{ item.description }}</p>
+        </div>
+      </template>
+    </authenticator>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+import { API } from "aws-amplify";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import { onCreateTodo } from "./graphql/subscriptions";
 
 export default {
-  name: 'App',
+  name: "App",
+  async created() {
+    this.getTodos();
+    this.subscribe();
+  },
   components: {
-    HelloWorld
-  }
-}
+    Authenticator,
+    // HelloWorld
+  },
+  data() {
+    return {
+      name: "",
+      description: "",
+      todos: [],
+    };
+  },
+  methods: {
+    async createTodo() {
+      const { name, description } = this;
+      if (!name || !description) return;
+      const todo = { name, description };
+      await API.graphql({
+        query: createTodo,
+        variables: { input: todo },
+      });
+      this.name = "";
+      this.description = "";
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos,
+      });
+      this.todos = todos.data.listTodos.items;
+    },
+    subscribe() {
+      API.graphql({ query: onCreateTodo }).subscribe({
+        next: (eventData) => {
+          let todo = eventData.value.data.onCreateTodo;
+          if (this.todos.some((item) => item.name === todo.name)) return; // remove duplications
+          this.todos = [...this.todos, todo];
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style>
